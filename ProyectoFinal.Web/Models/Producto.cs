468c5b299cf8921542a2a8d65ea353cc0ca68001
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2021-10-19 14:17:20 -0500

    Update models

--- ProyectoFinal.Web/Models/Producto.cs
+++ ProyectoFinal.Web/Models/Producto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 namespace ProyectoFinal.Web.Models
 {
     public class Producto
     {
+        [Key, ForeignKey("Subasta")]
         public int ProductoID { get; set; }
         public string Nombre{ get; set; }
         public string Descripcion{ get; set; }
