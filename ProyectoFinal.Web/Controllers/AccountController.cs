35ec64d0877105d81009f998922fd7650fcb3bab
Nestor Lozada <martinlozada1@gmail.com>
2021-10-23 12:24:07 -0500

    agrega comentario

--- ProyectoFinal.Web/Controllers/AccountController.cs
+++ ProyectoFinal.Web/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//a
 namespace ProyectoFinal.Web.Controllers
 {
     public class AccountController : Controller
