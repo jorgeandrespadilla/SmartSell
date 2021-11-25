488065ede704d6f655e6b3f07b74a49b8b35b215
Alain Ruales <alainmateo01@gmail.com>
2021-11-24 21:15:07 -0500

    Add "Edit view"

--- ProyectoFinal.Desktop/App.xaml.cs
+++ ProyectoFinal.Desktop/App.xaml.cs
@@ -69,7 +69,7 @@ namespace ProyectoFinal.Desktop
                     // Cuando no se restaura la pila de navegación, navegar a la primera página,
                     // configurando la nueva página pasándole la información requerida como
                     //parámetro de navegación
-                    rootFrame.Navigate(typeof(Login), e.Arguments);
+                    rootFrame.Navigate(typeof(Login),e.Arguments);
                 }
                 // Asegurarse de que la ventana actual está activa.
                 Window.Current.Activate();
