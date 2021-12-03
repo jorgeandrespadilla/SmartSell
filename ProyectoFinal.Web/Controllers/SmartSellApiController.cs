b3f3fcae75b779c9caa9e0767a70bf40423ee603
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2021-12-03 17:55:35 -0500

    Add shared project and add API to web project

---
+++ ProyectoFinal.Web/Controllers/SmartSellApiController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace ProyectoFinal.Web.Controllers
+{
+    public class SmartSellApiController : ApiController
+    {
+
+        [HttpGet]
+        [ActionName("Usuarios")]
+        public IHttpActionResult Test()
+        {
+            return Ok(new
+            {
+                x = 4,
+                y = "yolo"
+            });
+        }
+    }
+}
---
+++ ProyectoFinal.Web/Controllers/SubastasApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProyectoFinal.Web.Models;
+
+namespace ProyectoFinal.Web.Controllers
+{
+    public class SubastasApiController : ApiController
+    {
+        private SmartSell db = new SmartSell();
+
+        // GET: api/SubastasApi
+        public IQueryable<Subasta> GetSubasta()
+        {
+            return db.Subasta;
+        }
+
+        // GET: api/SubastasApi/5
+        [ResponseType(typeof(Subasta))]
+        public IHttpActionResult GetSubasta(int id)
+        {
+            Subasta subasta = db.Subasta.Find(id);
+            if (subasta == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subasta);
+        }
+
+        // PUT: api/SubastasApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSubasta(int id, Subasta subasta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != subasta.SubastaID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(subasta).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubastaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/SubastasApi
+        [ResponseType(typeof(Subasta))]
+        public IHttpActionResult PostSubasta(Subasta subasta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Subasta.Add(subasta);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = subasta.SubastaID }, subasta);
+        }
+
+        // DELETE: api/SubastasApi/5
+        [ResponseType(typeof(Subasta))]
+        public IHttpActionResult DeleteSubasta(int id)
+        {
+            Subasta subasta = db.Subasta.Find(id);
+            if (subasta == null)
+            {
+                return NotFound();
+            }
+
+            db.Subasta.Remove(subasta);
+            db.SaveChanges();
+
+            return Ok(subasta);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SubastaExists(int id)
+        {
+            return db.Subasta.Count(e => e.SubastaID == id) > 0;
+        }
+    }
+}
