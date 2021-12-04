c9cc7e0648fde340e0f559519d017e04f72119a6
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2021-12-04 14:25:54 -0500

    Add shared validator helper class

---
+++ ProyectoFinal.Shared/Helpers/Validator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ProyectoFinal.Shared.Helpers
+{
+    public class Validator
+    {
+        public static bool IsEmpty(string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (IsEmpty(email))
+            {
+                return false;
+            }
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        public static bool IsValidPassword(string password)
+        {
+            if (IsEmpty(password))
+            {
+                return false;
+            }
+            
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
