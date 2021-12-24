d8d645ce70d5c01d1c5174ab7b474659c06a082c
Alain Ruales <alainmateo01@gmail.com>
2021-12-24 12:54:23 -0500

    Add "Search string, MisSubastas, TodasSubastas, Filters" in IndexSubasta

--- ProyectoFinal.UWP/Views/IndexSubastasPage.xaml.cs
+++ ProyectoFinal.UWP/Views/IndexSubastasPage.xaml.cs
@@ -16,6 +16,7 @@ namespace ProyectoFinal.UWP.Views
     {
         private SmartSell smartSell = SmartSell.Instance;
         private ICollection<SubastaItem> subastasCargadas;
+        private string mode = "TodasSubastas";
 
         public IndexSubastasViewModel ViewModel { get; } = new IndexSubastasViewModel();
 
@@ -34,7 +35,6 @@ namespace ProyectoFinal.UWP.Views
         public void cargarSubastas()
         {
             subastas.ItemsSource = subastasCargadas;
-
         }
 
         private void subastas_SelectionChanged(object sender, ItemClickEventArgs e)
@@ -46,6 +46,46 @@ namespace ProyectoFinal.UWP.Views
         private void CrearSubastaHandler(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(CrearSubasta), null);
+        }
+
+        private async void MisSubastasHandlerBtn(object sender, RoutedEventArgs e)
+        {
+            todasSubastas.Visibility = Visibility.Visible;
+            misSubastas.Visibility = Visibility.Collapsed;
+            ocultarMisSubastas.Visibility = Visibility.Collapsed;
+            var resp = await smartSell.GetSubastas(showAll:"false",hideMySubastas:"false", searchString: buscarTxt.Text, hideEnded: ocultarFinalizadas.IsChecked.ToString().ToLower());
+            subastasCargadas = await smartSell.ConvertToSubastaItems(resp.Data);
+            mode = "MisSubastas";
+            cargarSubastas();
+            
+        }
+
+        private async void TodasSubastasHandlerBtn(object sender, RoutedEventArgs e)
+        {
+            todasSubastas.Visibility = Visibility.Collapsed;
+            misSubastas.Visibility = Visibility.Visible;
+            ocultarMisSubastas.Visibility = Visibility.Visible;
+            ocultarMisSubastas.IsChecked = true;
+            var resp = await smartSell.GetSubastas(showAll: "true", hideMySubastas: "true", searchString: buscarTxt.Text, hideEnded: ocultarFinalizadas.IsChecked.ToString().ToLower());
+            subastasCargadas = await smartSell.ConvertToSubastaItems(resp.Data);
+            mode = "TodasSubastas";
+            cargarSubastas();
+        }
+
+        private async void BuscarHandlerBtn(object sender, RoutedEventArgs e)
+        {
+            if(mode == "MisSubastas")
+            {
+                var resp = await smartSell.GetSubastas(searchString:buscarTxt.Text, showAll: "false", hideMySubastas: "false", hideEnded: ocultarFinalizadas.IsChecked.ToString().ToLower());
+                subastasCargadas = await smartSell.ConvertToSubastaItems(resp.Data);
+            }
+            else
+            {
+                var resp = await smartSell.GetSubastas(searchString: buscarTxt.Text, hideEnded: ocultarFinalizadas.IsChecked.ToString().ToLower() , showAll: "true", hideMySubastas: ocultarMisSubastas.IsChecked.ToString().ToLower());
+                subastasCargadas = await smartSell.ConvertToSubastaItems(resp.Data);
+            }
+            cargarSubastas();
+            
         }
     }
 }
