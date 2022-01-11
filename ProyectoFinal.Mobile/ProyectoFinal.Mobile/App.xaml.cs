d9aeb4bf4e791d7be4210c7f7bf963d62a32967e
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-01-11 17:07:29 -0500

    Add icons resources and configurations

--- ProyectoFinal.Mobile/ProyectoFinal.Mobile/App.xaml.cs
+++ ProyectoFinal.Mobile/ProyectoFinal.Mobile/App.xaml.cs
@@ -8,7 +8,6 @@ namespace ProyectoFinal.Mobile
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
--- ProyectoFinal.Mobile/ProyectoFinal.Mobile/AssemblyInfo.cs
+++ ProyectoFinal.Mobile/ProyectoFinal.Mobile/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("MaterialIconsRound-Regular.otf", Alias = "MaterialIcons")]
