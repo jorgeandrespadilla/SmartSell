7686d4908401910397c22cb47956d9755d88e648
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-01-23 01:12:16 -0500

    Add ofertas page functionality

--- ProyectoFinal.Mobile/ProyectoFinal.Mobile/Views/OfertasPage.xaml.cs
+++ ProyectoFinal.Mobile/ProyectoFinal.Mobile/Views/OfertasPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoFinal.Mobile.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,13 @@ namespace ProyectoFinal.Mobile.Views
         public OfertasPage()
         {
             InitializeComponent();
+            BindingContext = new OfertasViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((OfertasViewModel)BindingContext).Initialize();
         }
     }
 }
