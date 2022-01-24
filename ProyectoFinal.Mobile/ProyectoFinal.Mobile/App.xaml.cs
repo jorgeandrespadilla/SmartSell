f4b0f33010db4635db3b146386b4948fdb02cf6a
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-01-23 23:12:02 -0500

    Add Internet connectivity validation

--- ProyectoFinal.Mobile/ProyectoFinal.Mobile/App.xaml.cs
+++ ProyectoFinal.Mobile/ProyectoFinal.Mobile/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ProyectoFinal.Mobile.Services;
 using ProyectoFinal.Mobile.Views;
 using System;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,14 +18,32 @@ namespace ProyectoFinal.Mobile
 
         protected override void OnStart()
         {
+            ValidateConectivity();
+            Connectivity.ConnectivityChanged += ConnectivityChanged;
         }
 
         protected override void OnSleep()
         {
+            Connectivity.ConnectivityChanged -= ConnectivityChanged;
         }
 
         protected override void OnResume()
         {
+            Connectivity.ConnectivityChanged += ConnectivityChanged;
+        }
+
+        void ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            ValidateConectivity();
+        }
+
+        private async void ValidateConectivity()
+        {
+            var current = Connectivity.NetworkAccess;
+            if (current == NetworkAccess.None)
+            {
+                await Current.MainPage.DisplayAlert("Problemas de conectividad", "No se pudo acceder a la red actual. Compruebe su conexión a Internet.", "Aceptar");
+            }
         }
     }
 }
